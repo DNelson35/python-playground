ae1e81b7ddb19b3aa088977964db4f84c41bbf95
Damien Nelson <dhnisaman@gmail.com>
2024-01-07 11:49:37 -0500

    add function summon captain planet capitalize and add ! to each word in array

--- lib/cartoons.py
+++ lib/cartoons.py
@@ -1,3 +1,6 @@
+# no returns for functions print instead for ease of testing
+
+# It should then print out each name, in number order, using print.
 
 def roll_call_dwarves(l):
     if l :
@@ -5,6 +8,18 @@ def roll_call_dwarves(l):
             print(f"{i+1}. {l[i]}")
         return
     print("where are the dwarves?")
+
+
+# It should then capitalize each element and add an exclamation point at the end. The return value of this function should be a list,
+def summon_captain_planet(l):
+
+    refact = [el.capitalize() + "!" for el in l]
+
+    print(refact)
+
+
+
+
 
 
 
