cacdb7489b3027223285bfacbf6146c736bec9df
Damien Nelson <dhnisaman@gmail.com>
2024-02-04 17:06:02 -0500

    create an executable file

---
+++ lib/labs/ex_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# import sys  the line above makes file executable and sys allows us to call sys.argv to pass an argument at the command line when this file is executed
+
+## so typing from the labs directory "./ex_file.py Damien" would pass damein in as the argument the response would be "The name is Damien"
+
+# name = sys.argv[1]
+
+# if __name__ == '__main__':
+#     print(f"The name is {name}")
+
+import os
+
+if __name__ == "__main__":
+    os.system('ls -l')
+
+
+    
