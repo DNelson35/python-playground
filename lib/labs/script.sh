f151e7e82ec4501fe58b1608dfd4c0c25dbed15a
Damien Nelson <dhnisaman@gmail.com>
2024-02-04 17:12:35 -0500

    create bash script

---
+++ lib/labs/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# A Bash script is a text file which can contains a series of commands. These commands can help us automate tasks that would require the developer to type multiple commands in the terminal. Any task you can run in the terminal can be put in a Bash script. All you need to do is add the commands sequentially in the Bash file. For Bash scripts the convention is to use the .sh file extension.
+
+echo "Files in this directory"
+
+pwd
