ece2d1359255617753addcf3014f3b00825e73bb
Damien Nelson <dhnisaman@gmail.com>
2024-02-08 21:25:51 -0500

    remove some comments

--- lib/labs/ex_file.py
+++ lib/labs/ex_file.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
-# import sys  the line above makes file executable and sys allows us to call sys.argv to pass an argument at the command line when this file is executed
+import sys  ##the line above makes file executable and sys allows us to call sys.argv to pass an argument at the command line when this file is executed
 
 ## so typing from the labs directory "./ex_file.py Damien" would pass damein in as the argument the response would be "The name is Damien"
 
-# name = sys.argv[1]
+name = sys.argv[1]
 
-# if __name__ == '__main__':
-#     print(f"The name is {name}")
+if __name__ == '__main__':
+    i = 100
+    while i >  0:
+        print(f"The name is {name}")
+        i -= 1
 
-import os
+# import os
 
-if __name__ == "__main__":
-    os.system('ls -l')
+# if __name__ == "__main__":
+#     os.system('ls -l')
 
 
     
