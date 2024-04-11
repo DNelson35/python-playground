8f1a5392972e2db504f08a2adb1dcaee45387d69
Damien Nelson <dhnisaman@gmail.com>
2024-04-10 22:39:25 -0400

    no change

--- lib/challenges/spin_words.py
+++ lib/challenges/spin_words.py
@@ -2,3 +2,5 @@ def spin_words(string):
     return ' '.join([word[::-1] if len(word) >=  5 else word for word in string.split(' ')])
 
 print(spin_words("this should be called somthing better"))
+
+
