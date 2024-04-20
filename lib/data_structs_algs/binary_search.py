636a6decd00f32774c42b39c879b8db5772b2d67
Damien Nelson <dhnisaman@gmail.com>
2024-04-20 19:43:48 -0400

    binary search reWrite

--- lib/data_structs_algs/binary_search.py
+++ lib/data_structs_algs/binary_search.py
@@ -1,50 +1,60 @@
 import math
 
-# second write
-def test_location(arr, num, mid):
-    if arr[mid] == num:
-        if mid-1 >= 0 and arr[mid-1] == num:
-            return 'left'
-        else:
-            return 'found'
-    elif arr[mid] < num:
-        return 'left'
-    else:
-        return 'right'
-
-# first write
+# second write 
 # def test_location(arr, num, mid):
-#     mid_number = arr[mid]
-#     print('mid:', mid, ", mid_number:", mid_number)
-#     if mid_number == num:
+#     if arr[mid] == num:
 #         if mid-1 >= 0 and arr[mid-1] == num:
 #             return 'left'
 #         else:
 #             return 'found'
-#     elif mid_number < num:
+#     elif arr[mid] < num:
 #         return 'left'
 #     else:
 #         return 'right'
 
-def binary_search(arr, num): # O(log n)
-    lo, hi = 0, len(arr) - 1
+
+# def binary_search(arr, num): # O(log n)
+#     lo, hi = 0, len(arr) - 1
+#     while lo <= hi:
+#         mid = (lo + hi) // 2
+#         result = test_location(arr, num, mid)
+#         if result == 'found':
+#             return mid
+#         elif result == 'left':
+#             hi = mid -1
+#         elif result == 'right':
+#             lo = mid + 1   
+#     return -1
+
+# third write
+
+def binary_search(lo, hi, condition):
     while lo <= hi:
         mid = (lo + hi) // 2
-        result = test_location(arr, num, mid)
+        result = condition(mid)
         if result == 'found':
             return mid
         elif result == 'left':
-            hi = mid -1
-        elif result == 'right':
-            lo = mid + 1   
+            hi = mid - 1
+        else:
+            lo = mid + 1
     return -1
 
 
+def locate_num(arr, num):
+    def condition(mid):
+        if arr[mid] == num:
+            if mid > 0 and arr[mid - 1] == num:
+                return 'left'
+            else:
+                return 'found'
+        elif arr[mid] < num:
+            return 'left'
+        else:
+            return 'right'
 
-    
+    return binary_search(0, len(arr) - 1, condition)
 
-  
-    
 
 
 
